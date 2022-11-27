fba575ec66cb92e1fde502227a92afa56a5b3197
SEYUN <87846913+seyun24@users.noreply.github.com>
2022-11-27 20:58:19 +0900

    Update database.js

--- server_node/config/database.js
+++ server_node/config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: 'database-1.c86beyinvows.ap-northeast-2.rds.amazonaws.com',
-    user: 'admin',
-    port: '3306',
-    password: 'aa192837',
-    database: 'test'
+    host: '',
+    user: '',
+    port: '',
+    password: '',
+    database: ''
 });
 
 module.exports = {
